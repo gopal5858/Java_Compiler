e50c28abb3d3b71b3a6c5edd06bdf691403ab232
gopal5858 <85783691+gopal5858@users.noreply.github.com>
2023-08-22 11:42:45 +0530

    Submittion Upload

---
+++ tests/test_n1.java
@@ -0,0 +1,22 @@
+public class next{
+  public int foo(){
+    return 99;
+  }
+}
+public class CrewMember {
+  int lol=8;
+    public static void main() {
+        int a =5;
+        while(a>0){
+          System.out.println(a);
+          a--;
+        }
+        do {
+          a++;
+          System.out.println(a);
+        }while(a<5);
+        int b = foo();
+        System.out.println(b);
+
+    }
+}
